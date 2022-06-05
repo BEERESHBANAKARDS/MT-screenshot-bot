8c452b312cbfe0b843ceb41ebda7a5d85988519b
BEERESHBANAKARDS <88141192+BEERESHBANAKARDS@users.noreply.github.com>
2022-06-05 18:18:58 +0530

    Update screenshotbot.py

--- bot/screenshotbot.py
+++ bot/screenshotbot.py
@@ -19,7 +19,7 @@ log = logging.getLogger(__name__)
 
 class ScreenShotBot(Client):
     def __init__(self):
-        super().__init__
+        super().__init__(
             session_name=Config.SESSION_NAME,
             bot_token=Config.BOT_TOKEN,
             api_id=Config.API_ID,
